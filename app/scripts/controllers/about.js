9f439ad403ed9111d3ec2ea343681ec0a0da129a
yash kochar <kochar.yash@gmail.com>
2016-12-09 15:29:36 +0530

    angular installed

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name audioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the audioApp
+ */
+angular.module('audioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name audioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the audioApp
+ */
+angular.module('audioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
