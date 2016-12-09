6a2ec4dc05e760acf17b96da69e4b4de1e8a7a04
yash kochar <kochar.yash@gmail.com>
2016-12-09 18:11:17 +0530

    using ngAudio 2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,13 @@
  * Controller of the audioApp
  */
 angular.module('audioApp')
-  .controller('MainCtrl', function (ngAudio) {
+  .controller('MainCtrl', function ($scope, ngAudio, $timeout) {
+    var audio = null;
     var load = function () {
-        var audio = ngAudio.load('https://s3-ap-southeast-1.amazonaws.com/lgwarehouse/media/resources/slide/4486/Water-chemical-properties-17.mp3');
-        audio.play();
+        audio = ngAudio.load('https://s3-ap-southeast-1.amazonaws.com/lgwarehouse/media/resources/slide/4486/Water-chemical-properties-17.mp3');
+        $timeout(function () {
+            audio.play();
+        },1000);
     }();
 
     $scope.play = function () {
