c6d93cb8ffaa1584032ed306595332d2a884f2ea
yash kochar <kochar.yash@gmail.com>
2016-12-12 15:59:53 +0530

    auto slide

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,7 @@ angular.module('audioApp')
   .controller('MainCtrl', function ($scope, ngAudio, $timeout) {
     var audios = [];
     var play_timeout = null;
+    var audio_duration = 0;
 
     $scope.slides = [{
         image: 'https://s3-ap-southeast-1.amazonaws.com/lgwarehouse/media/resources/slide/4486/Water-(chemical properties)-04.jpg',
@@ -21,28 +22,29 @@ angular.module('audioApp')
     },{
         image: 'https://s3-ap-southeast-1.amazonaws.com/lgwarehouse/media/resources/slide/4486/Water-(chemical properties)-30.jpg',
         audio: 'https://s3-ap-southeast-1.amazonaws.com/lgwarehouse/media/resources/slide/4486/Water-chemical-properties-30.mp3'
-    },{
-        image: 'https://s3-ap-southeast-1.amazonaws.com/lgwarehouse/media/resources/slide/4486/resources/slide/4486/Water-(chemical properties)-01.jpg',
-        audio: 'https://s3-ap-southeast-1.amazonaws.com/lgwarehouse/media/resources/slide/4486/Water-chemical-properties-01.mp3'
     }];
 
     var load = function () {
-        for(var i=0; i<4; i++) {
+        for(var i=0; i<3; i++) {
             audios.push(ngAudio.load($scope.slides[i].audio));
         }
-        $timeout(function () {
-            audios[0].play();
-        },1000);
     }();
 
     $scope.$watch('active', function (currentIndex, previousIndex) {
         $timeout.cancel(play_timeout);
-        var audio_duration = 1000 * audios[currentIndex].duration;
-        audios[previousIndex].stop();
+        console.log(audios[currentIndex]);
+        if(audios[currentIndex] !== undefined) {
+            audio_duration = 1000 * parseFloat(audios[currentIndex].duration);
+            console.log(audios[currentIndex].duration, audio_duration);
+        }
+        if(audios[previousIndex] !== undefined) {
+            audios[previousIndex].stop();
+        }
         play_timeout = $timeout(function () {
             audios[currentIndex].play();
+            // console.log($scope.interval);
         }, 500);
-        $scope.interval = parseInt(5000 + audio_duration + 500);
+        $scope.interval = parseInt(10000);
     });
 
     $scope.play = function () {
